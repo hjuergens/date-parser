52e03a0b334ca0dd2800ec5ff4dcaeb5c1527453
Hartmut Jürgens <hartmut_juergens@yahoo.de>
2015-06-13 13:42:55 +0200

    st

---
+++ src/main/scala/de/juergens/App.scala
@@ -0,0 +1,14 @@
+package de.juergens
+
+/**
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}

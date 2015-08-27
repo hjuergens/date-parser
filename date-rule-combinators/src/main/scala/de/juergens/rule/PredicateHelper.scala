09a8b73b6526d2d59dca01f5c85b3f244de3a46c
hjuergens <hartmut_juergens@yahoo.de>
2015-08-27 11:04:11 +0200

    tests revised

--- date-rule-combinators/src/main/scala/de/juergens/rule/PredicateHelper.scala
+++ date-rule-combinators/src/main/scala/de/juergens/rule/PredicateHelper.scala
@@ -4,13 +4,37 @@ package de.juergens.rule
 import java.util.function.Predicate
 import scala.languageFeature.implicitConversions
 
+/**
+ * [[Predicate]]
+ * @constructor create a predicates as or composition
+ * @param predicates any predicates on type T
+ * @tparam T
+ */
 case class UnionPredicate[T](predicates: Predicate[T]*) extends Predicate[T] {
+
+  /**
+   * Checks if any of the predicates matches for the specified object.
+   * @param t an object to test
+   * @return or-composition
+   */
   def test(t: T) = predicates.foldLeft(false)((b, r) => b | r.test(t))
 
   override def toString = predicates.mkString("|")
 }
 
+/**
+ * [[Predicate]]
+ * @constructor create a predicates as and composition
+ * @param predicates tany predicates on type T
+ * @tparam T
+ */
 case class IntersectionPredicate[T](predicates: Predicate[T]*) extends Predicate[T] {
+
+  /**
+   * Checks if all of the predicates matches for the specified object.
+   * @param t an object to test
+   * @return and-composition
+   */
   def test(t: T) = predicates.foldRight(true)((r, b) => b & r.test(t))
 
   override def toString = predicates.mkString("&")
--- date-rule-combinators/src/main/scala/de/juergens/time/TimeUnit.scala
+++ date-rule-combinators/src/main/scala/de/juergens/time/TimeUnit.scala
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2015 Hartmut Jürgens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,10 +22,10 @@
 package de.juergens.time
 
 import java.time.temporal._
-import de.juergens.time.{Date => _ }
+import de.juergens.time.{DateHelper => _ }
 import java.time.Duration
 
-import de.juergens.time.{Date => _}
+import de.juergens.time.{DateHelper => _}
 
 /**
  *

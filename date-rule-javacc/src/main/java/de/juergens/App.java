01853eab9074328194bf81ab1f25439703b85b75
juergens <hjuergens@arcor.de>
2015-08-31 21:08:06 +0200

    add empty project for javacc

---
+++ date-rule-javacc/src/main/java/de/juergens/App.java
@@ -0,0 +1,13 @@
+package de.juergens;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

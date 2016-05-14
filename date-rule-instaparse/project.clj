bf1c276546bef750873605d751f5254b9a4ae1c8
juergens <hjuergens@arcor.de>
2016-05-14 15:26:12 +0200

    some experimentation with instaparse

--- date-rule-instaparse/project.clj
+++ date-rule-instaparse/project.clj
@@ -3,5 +3,6 @@
   :url "http://hjuergens.github.io/date-parser/date-rule-instaparse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"] [instaparse "1.4.1"]]
-  :main date-rule-instaparse.core)
+  :dependencies [[org.clojure/clojure "1.7.0"] [instaparse "1.4.2"] [rhizome "0.2.5"]]
+  :main date-rule-instaparse.core
+  :aot [date-rule-instaparse.core])
--- date-rule-instaparse/src/date_rule_instaparse/core.clj
+++ date-rule-instaparse/src/date_rule_instaparse/core.clj
@@ -1,9 +1,7 @@
 (ns date-rule-instaparse.core
   (:require [instaparse.core :as insta])
-  (:gen-class))
-
-(ns date-rule-instaparse.core
-    (:gen-class))
+  (:gen-class)
+  (:import (java.time Year)))
 
 (def as-and-bs
   (insta/parser
@@ -12,7 +10,39 @@
      A = 'a'+
      B = 'b'+"))
 
+(def arithmetic
+  (insta/parser
+    "expr = add-sub
+     <add-sub> = mul-div | add | sub
+     add = add-sub <'+'> mul-div
+     sub = add-sub <'-'> mul-div
+     <mul-div> = term | mul | div
+     mul = mul-div <'*'> term
+     div = mul-div <'/'> term
+     <term> = number | <'('> add-sub <')'>
+     number = #'[0-9]+'"))
+
+(Year/parse "2003")
+
+(def year
+  (insta/parser
+    "year = #'[1-9]\\d{3}' | twodigityear
+    twodigityear = #'[0-9]{2}'"))
+
+(insta/transform
+  {:year #(Year/parse %1)}
+  (year "1999"))
+
+(defn parseYear
+  "Parse string to year."
+  [^String s]
+  (insta/transform
+    {:year #(java.time.Year/parse %1)}
+    ((insta/parser "year = #'[1-9]\\d{3}'") s)))
+
 (defn -main
     "I don't do a whole lot...yet."
     [& args]
-    ((insta/visualize (as-and-bs "aaabbab") :output-file "vizexample1.png" :options {:dpi 63})))
+    (insta/visualize (as-and-bs "aaabbab")
+                     :output-file "vizexample1.png"
+                      :options {:dpi 63}))

66354a7a0c04a03e91cf771038abf433fef1f992
juergens <hjuergens@arcor.de>
2017-04-05 19:04:03 +0200

    implements List with Visitor

--- date-rule-antlr/src/test/java/io/github/hjuergens/time/ListParserTest.java
+++ date-rule-antlr/src/test/java/io/github/hjuergens/time/ListParserTest.java
@@ -10,21 +10,33 @@ import java.util.List;
 import static org.testng.Assert.assertEquals;
 
 public class ListParserTest {
+
     @Test
     public void testList() throws Exception {
         ANTLRInputStream inputStream = new ANTLRInputStream("(3:5:14)");
         ListLexer lexer = new ListLexer(inputStream);
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-        ListParser lp = new ListParser(tokenStream);
-        List<Integer> expected = new ArrayList<>(12);
-        expected.add(3+0*5);
-        expected.add(3+1*5);
-        expected.add(3+2*5);
+        ListParser parser = new ListParser(tokenStream);
 
-        final List<Integer> actual = lp.list().listOut;
+        List<Integer> expected = new ArrayList<Integer>() {
+            {
+                add(3+0*5);
+                add(3+1*5);
+                add(3+2*5);
+            }
+        };
+
+        final List<Integer> actual = new IntegerListVisitor().visit(parser.list());
+
         assertEquals( actual, expected);
         assertEquals( actual.toArray(), new Integer[]{3,8,13});
     }
 
-
+    @Test
+    public void testVisitor() {
+        ListLexer lexer = new ListLexer(new ANTLRInputStream("(2:7:23)"));
+        ListParser parser = new ListParser(new CommonTokenStream(lexer));
+        List<Integer> sum = new IntegerListVisitor().visit(parser.list());
+        assertEquals(sum.toString(), "[2, 9, 16, 23]");
+    }
 }

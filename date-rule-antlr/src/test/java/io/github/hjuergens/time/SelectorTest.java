ced1576a94231f60ce9f0243859f8f3ec4ae3f2c
juergens <hjuergens@arcor.de>
2018-06-03 11:14:02 +0200

    adjust one method to java.time

--- date-rule-antlr/src/test/java/io/github/hjuergens/time/SelectorTest.java
+++ date-rule-antlr/src/test/java/io/github/hjuergens/time/SelectorTest.java
@@ -1,6 +1,14 @@
 package io.github.hjuergens.time;
 
 import org.joda.time.DateTime;
+
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
 import org.joda.time.DateTimeConstants;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
@@ -15,7 +23,7 @@ public class SelectorTest {
     private static LocalTime twentyFour = zero.minusMillis(1);
 
     @Test
-    public void testNextQuarter_0() throws Exception {
+    public void testNextQuarter_0() {
         DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule(">Q").next();
 
         DateTime referenceDateTime = new DateTime(2016, 12, 5, 15, 18, 22, 65);
@@ -26,7 +34,7 @@ public class SelectorTest {
     }
 
     @Test
-    public void testNextQuarter_1() throws Exception {
+    public void testNextQuarter_1() {
         DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule(">Q").next();
 
         {
@@ -47,7 +55,7 @@ public class SelectorTest {
     }
 
     @Test(enabled = false)
-    public void testNextQuarter_2() throws Exception {
+    public void testNextQuarter_2() {
         DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule(">=Q").next();
 
 
@@ -61,7 +69,7 @@ public class SelectorTest {
     }
 
     @Test
-    public void testNextWednesday() throws Exception {
+    public void testNextWednesday() {
         DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule(">wednesday").next();
 
         DateTime referenceDateTime = new DateTime(2016, 12, 5, 15, 18, 22, 65);
@@ -75,7 +83,7 @@ public class SelectorTest {
     }
 
     @Test
-    public void testThirdWednesday() throws Exception {
+    public void testThirdWednesday() {
         DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule(">>>wednesday").next();
 
         DateTime referenceDateTime = new DateTime(2016, 12, 5, 15, 18, 22, 65);
@@ -90,7 +98,36 @@ public class SelectorTest {
     }
 
     @Test
-    public void testNextDay() throws Exception {
+    public void testThirdWednesday8() {
+        DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule(">>>wednesday").next();
+
+        LocalDateTime referenceDateTime = LocalDateTime.of(2016, 12, 5, 15, 18, 22, 65);
+        DateTime jodasReferenceDateTime = new DateTime(2016, 12, 5, 15, 18, 22, 65);
+        DateTime actual = adjuster.adjustInto(jodasReferenceDateTime);
+
+        java.time.LocalTime zero = java.time.LocalTime.of(0,0,0,0);
+        TemporalAdjuster thirdWednesday = new TemporalAdjuster() {
+            private TemporalAdjuster w = TemporalAdjusters.next(DayOfWeek.WEDNESDAY);
+            @Override
+            public Temporal adjustInto(Temporal temporal) {
+                Temporal rtnTemporal = temporal;
+                for (int i = 0; i < 3; i++) {
+                    rtnTemporal = w.adjustInto(rtnTemporal);
+
+                }
+                return rtnTemporal;
+            }
+        };
+        LocalDateTime expectation = LocalDateTime.from(thirdWednesday.adjustInto(referenceDateTime)).with(zero);
+        assertEquals( actual.getYear(), expectation.getYear(), adjuster.toString() );
+        assertEquals( actual.getMonthOfYear(), expectation.getMonth().getValue(), adjuster.toString() );
+        assertEquals( actual.getDayOfMonth(), expectation.getDayOfMonth(), adjuster.toString() );
+        assertEquals( actual.getHourOfDay(), expectation.getHour(), adjuster.toString() );
+        assertEquals( actual.getMinuteOfHour(), expectation.getMinute(), adjuster.toString() );
+    }
+
+    @Test
+    public void testNextDay() {
         DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule(">D").next();
 
         DateTime referenceDateTime = new DateTime(2016, 2, 28, 15, 18, 22, 65);
@@ -103,7 +140,7 @@ public class SelectorTest {
     }
 
     @Test
-    public void testPreviousDay() throws Exception {
+    public void testPreviousDay() {
         DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule("<<D").next();
 
         DateTime referenceDateTime = new DateTime(2016, 1, 2, 15, 18, 22, 65);
@@ -116,7 +153,7 @@ public class SelectorTest {
     }
 
     @Test
-    public void testNextButOneMonth() throws Exception {
+    public void testNextButOneMonth() {
         DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule(">>M").next();
 
         DateTime referenceDateTime = new DateTime(2016, 1, 2, 15, 18, 22, 65);
@@ -130,7 +167,7 @@ public class SelectorTest {
 
 
     @Test(enabled = true, dependsOnMethods = { "testThirdWednesday", "testNextButOneMonth" })
-    public void testThirdWedesdayInSecondMonth() throws Exception {
+    public void testThirdWedesdayInSecondMonth() {
         DateTimeAdjuster adjuster = SchedulerFactory.parseSchedule(">>M>>>wednesday").next();
 
         DateTime referenceDateTime = new LocalDate(2018,1,1).toDateTime(LocalTime.now());

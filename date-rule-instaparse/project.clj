9aa1022d7449e8d25ab081428087a41dcccd721c
juergens <hjuergens@arcor.de>
2019-06-09 19:56:17 +0200

    make it run again

--- date-rule-instaparse/project.clj
+++ date-rule-instaparse/project.clj
@@ -5,12 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [instaparse "1.4.8"]
+                 [instaparse "1.4.9"]
                  [rhizome "0.2.9"]
                  [clojure.java-time "0.3.2"]
                  [org.threeten/threeten-extra "1.3.2"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [clj-antlr "0.2.4"]]
   :main date-rule-instaparse.core
   :aot [date-rule-instaparse.core]
   :plugins [[lein-ring "0.9.7"]]
